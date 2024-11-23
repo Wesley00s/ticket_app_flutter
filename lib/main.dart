ac66d147fcb9e29ca4d3669ef8640fca4637fbba
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-23 14:55:38 -0300

    refactor: config routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/bottom_nav_bar.dart';
+import 'package:ticket_app/screens/home/all_hotels_screen.dart';
+import 'package:ticket_app/screens/home/all_tickets_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'Book Tickets',
       debugShowCheckedModeBanner: false,
-      home: BottomNavBar()
+      routes: {
+        "/": (context) => const BottomNavBar(),
+        "/all_tickets_screen": (context) => const AllTicketsScreen(),
+        "/all_hotels_screen": (context) => const AllHotelsScreen(),
+      },
     );
   }
 }
