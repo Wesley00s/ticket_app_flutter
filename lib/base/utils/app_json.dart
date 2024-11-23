dfa64892318b56c323e4168dbc3c08816ccc4532
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-23 15:00:06 -0300

    feat: create any map data to display dynamic on app

---
+++ lib/base/utils/app_json.dart
@@ -0,0 +1,177 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New-York"
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23
+  },
+  {
+    'from': {
+      'code': "DK",
+      'name': "Dhaka"
+    },
+    'to': {
+      'code': "SH",
+      'name': "Shanghai"
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time': "09:00 AM",
+    "number": 45
+  },
+  {
+    'from': {
+      'code': "SYD",
+      'name': "Sydney"
+    },
+    'to': {
+      'code': "SIN",
+      'name': "Singapore"
+    },
+    'flying_time': '8H 10M',
+    'date': "15 JUN",
+    'departure_time': "10:00 AM",
+    "number": 78
+  },
+  {
+    'from': {
+      'code': "BOM",
+      'name': "Mumbai"
+    },
+    'to': {
+      'code': "DXB",
+      'name': "Dubai"
+    },
+    'flying_time': '3H 50M',
+    'date': "20 JUL",
+    'departure_time': "06:30 PM",
+    "number": 12
+  },
+  {
+    'from': {
+      'code': "LAX",
+      'name': "Los Angeles"
+    },
+    'to': {
+      'code': "TYO",
+      'name': "Tokyo"
+    },
+    'flying_time': '11H 15M',
+    'date': "5 AUG",
+    'departure_time': "11:45 PM",
+    "number": 34
+  },
+  {
+    'from': {
+      'code': "JNB",
+      'name': "Johannesburg"
+    },
+    'to': {
+      'code': "CAI",
+      'name': "Cairo"
+    },
+    'flying_time': '7H 25M',
+    'date': "12 SEP",
+    'departure_time': "01:00 PM",
+    "number": 56
+  },
+  {
+    'from': {
+      'code': "PAR",
+      'name': "Paris"
+    },
+    'to': {
+      'code': "BER",
+      'name': "Berlin"
+    },
+    'flying_time': '1H 40M',
+    'date': "18 OCT",
+    'departure_time': "07:15 AM",
+    "number": 89
+  },
+  {
+    'from': {
+      'code': "MEX",
+      'name': "Mexico City"
+    },
+    'to': {
+      'code': "SCL",
+      'name': "Santiago"
+    },
+    'flying_time': '8H 50M',
+    'date': "25 NOV",
+    'departure_time': "02:00 PM",
+    "number": 67
+  }
+];
+
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotel_room.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'city_view.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'pool_view.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+  {
+    'image': 'hotel_room_1.png',
+    'place': 'Ocean Breeze',
+    'destination': 'Sydney',
+    'price': 55
+  },
+  {
+    'image': 'hotel_room_view_1.png',
+    'place': 'Mountain Retreat',
+    'destination': 'Kathmandu',
+    'price': 30
+  },
+  {
+    'image': 'hotel_room_view_2.png',
+    'place': 'City Lights',
+    'destination': 'New York',
+    'price': 85
+  },
+  {
+    'image': 'hotel_room_view_3.png',
+    'place': 'Safari Lodge',
+    'destination': 'Nairobi',
+    'price': 45
+  },
+  {
+    'image': 'pool_view_1.png',
+    'place': 'Paradise Resort',
+    'destination': 'Maldives',
+    'price': 120
+  },
+  {
+    'image': 'pool_view_2.png',
+    'place': 'Historic Inn',
+    'destination': 'Rome',
+    'price': 60
+  },
+  {
+    'image': 'pool_view_3.png',
+    'place': 'Luxury Haven',
+    'destination': 'Paris',
+    'price': 95
+  }
+];
