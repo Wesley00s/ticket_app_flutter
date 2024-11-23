edc6a99b90fe00da025646d119c6f07a64e45cc5
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-23 14:53:27 -0300

    refactor: add new attribute 'is color' in big dot widget

--- lib/base/widgets/big_dot.dart
+++ lib/base/widgets/big_dot.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class BigDot extends StatelessWidget {
-  const BigDot({super.key});
+  final bool isColor;
+  const BigDot({super.key, this.isColor = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(3),
       decoration: BoxDecoration(
-        border: Border.all(width: 2.5, color: Colors.white),
-        borderRadius: BorderRadius.circular(20)
-      ),
+          border: Border.all(
+            width: 2.5,
+            color: !isColor ? Colors.white : const Color(0xFF4DB5CB),
+          ),
+          borderRadius: BorderRadius.circular(20)),
     );
   }
 }
